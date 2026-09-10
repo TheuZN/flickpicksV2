7acf13c27748f19b5a8e1bf62a70d71e7e2fea6a
Mateus Melo <mateus.gon.melo@gmail.com>
2026-09-10 18:43:50 -0300

    ajuste vercel deploy

---
+++ src/environments/enviroment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  linkUrl: 'https://api.themoviedb.org/3',
+  linkImageUrl: 'https://image.tmdb.org/t/p/w500',
+  acessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYzA2ZTRlZjZlM2Y0ZmVhYTMzZDQ0Mjg4OGE4NjUwMyIsInN1YiI6IjY0YjU1YjZlMzc4MDYyMDBhZDE1YTYwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r02wz2QKgrirlour_iEOzI0Po2VeCehBJZEw6pkYe1Q',
+};
